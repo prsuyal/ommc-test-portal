b78ad83174fccc7054919b60f7a2341e8b70b5ce
Pranshu Suyal <suyalpranshu@gmail.com>
2025-05-16 11:05:47 -0400

    fixed mobile styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,9 @@ import Navbar from "./components/navbar";
 import TestPortal from "../portal/portal";
 import { CursorClickIcon } from "@heroicons/react/solid";
 import Head from "next/head";
-import { useState, useEffect } from "react";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
 
   return (
     <>
@@ -25,50 +20,46 @@ const Home: NextPage = () => {
         {/* {!session?.user ? ( */}
         <div className="relative z-10 flex  h-[calc(100vh-3.58rem)] w-full flex-col items-center justify-center text-3xl font-extrabold ">
           <div className="z-5 pattern-opacity-70 pattern-cross absolute h-[100vh] w-[100vw] duration-150 pattern-bg-gray-100 pattern-gray-300 pattern-size-8 dark:pattern-gray-800 dark:pattern-bg-gray-900"></div>
-          {isMounted && (
-            <>
-              <Image
-                alt="Background"
-                src="/images/portal-light.png"
-                className="z-5 absolute h-full w-full object-cover opacity-20 blur-[3px] dark:hidden"
-                priority
-                height={980}
-                width={980}
-              />
-              <Image
-                alt="Background"
-                src="/images/portal-dark.png"
-                className="z-5 absolute hidden h-full w-full object-cover opacity-20 blur-[3px] dark:inline"
-                priority
-                height={980}
-                width={980}
-              />
-            </>
-          )}
+          <Image
+            alt="Background"
+            src="/images/portal-light.png"
+            className="z-5 absolute h-full w-full object-cover opacity-20 blur-[3px] dark:hidden"
+            priority
+            height={980}
+            width={980}
+          />
+          <Image
+            alt="Background"
+            src="/images/portal-dark.png"
+            className="z-5 absolute hidden h-full w-full object-cover opacity-20 blur-[3px] dark:inline"
+            priority
+            height={980}
+            width={980}
+          />
           <h5 className="z-10 text-center text-3xl  font-extrabold sm:text-4xl md:text-5xl lg:mb-8 lg:text-6xl xl:text-7xl">
             Welcome to{" "}
             <span className="block rounded-3xl bg-red-600 px-3 py-1 text-white dark:bg-red-700 sm:inline">
               OMMC Year 5
             </span>
           </h5>
-          {/* <Link
+          <Link
             href="https://www.ommcofficial.org/"
             className="relative z-10 px-8 py-4 text-center text-xl font-[600] duration-150 hover:scale-[1.02] hover:underline"
           >
             <span className="mr-2 mb-2 block rounded-3xl bg-yellow-500 px-3 py-1 text-gray-900 sm:inline">
-              ATTENTION!
-            </span>{" "}
-            The first round of OMMC 2025 is over. Stay tuned for updates!
-          </Link> */}
-          <Link
-            href="https://www.ommcofficial.org/"
-            className="relative z-10 text-center text-xl font-[600] duration-150 hover:scale-[1.02] hover:underline"
-          >
-            <span className="mr-2 block rounded-3xl bg-yellow-500 px-3 py-1 text-gray-900 sm:inline">
               COMING SOON!
             </span>{" "}
             Stay tuned for new updates.
           </Link>
+          {/* <Link
+            href="https://www.ommcofficial.org/"
+            className="relative z-10 text-center text-xl font-[600] duration-150 hover:scale-[1.02] hover:underline"
+          >
+            <span className="mr-2 block rounded-3xl bg-yellow-500 px-3 py-1 text-gray-900 sm:inline">
+              ATTENTION!
+            </span>{" "}
+            The first round of OMMC 2024 is over. Stay tuned for updates!
+          </Link> */}
           {/* <button
               onClick={() => void signIn()}
               className="z-10 mt-8 flex select-none flex-row items-center rounded-2xl p-4 text-xl font-[600] duration-150 hover:scale-[1.02] hover:underline lg:text-3xl"
@@ -76,7 +67,9 @@ const Home: NextPage = () => {
               <CursorClickIcon className="mr-2 h-7 w-7" /> Register to Submit
             </button> */}
         </div>
-        {/* {isMounted ? <TestPortal /> : null} */}
+        {/* ) : (
+          <TestPortal />
+        )} */}
       </main>
     </>
   );
